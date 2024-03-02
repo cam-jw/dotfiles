e9a23b5d5d780127f70078c3befedec48453d866
cam-jw <cam.jw@outlook.com>
2024-03-02 19:39:21 +0000

    Alter lualine to show absolute location/progress

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,57 +1,29 @@
+-- vim-based function to get the current and total lines
+local function get_location()
+    local current_line = vim.fn.line(".")
+    local total_lines = vim.fn.line("$")
+    return string.format("%d/%d", current_line, total_lines)
+end
+
 return {
     "nvim-lualine/lualine.nvim",
-    event = { "InsertEnter", "CmdLineEnter" },
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
 
     opts = function(_, opts)
-        local function show_macro_recording()
-            local recording_register = vim.fn.reg_recording()
-            if recording_register == "" then
-                return ""
-            else
-                return "Recording @" .. recording_register
-            end
-        end
-
-        vim.api.nvim_create_autocmd("RecordingEnter", {
-            callback = function()
-                require("lualine").refresh({
-                    place = { "statusline" },
-                })
-            end,
-        })
-
-        vim.api.nvim_create_autocmd("RecordingLeave", {
-            callback = function()
-                local timer = vim.loop.new_timer()
-                timer:start(
-                    50,
-                    0,
-                    vim.schedule_wrap(function()
-                        require("lualine").refresh({
-                            place = { "statusline" },
-                        })
-                    end)
-                )
-            end,
-        })
-
         opts.options = {
             icons_enabled = false,
             theme = "auto",
             component_separators = { left = "", right = "" },
             section_separators = { left = "", right = "" },
             disabled_filetypes = {
-                inactive_winbar = {},
-                statusline = { "alpha", "dashboard", "fzf", "lazy", "mason", "TelescopePrompt" },
-                tabline = {},
+                statusline = {},
                 winbar = {},
             },
             ignore_focus = {},
             always_divide_middle = true,
-            globalstatus = true,
+            globalstatus = vim.go.laststatus == 3,
             refresh = {
                 statusline = 1000,
                 tabline = 1000,
@@ -61,8 +33,21 @@ return {
 
         opts.sections = {
             lualine_a = { "mode" },
-            lualine_b = { "filename" },
-            lualine_c = { "branch" },
+            lualine_b = { "branch" },
+            lualine_c = { { "filename", path = 1 } },
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {
+                {
+                    "location",
+                    fmt = get_location,
+                },
+            },
+        }
+        opts.inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { { "filename", path = 1 } },
             lualine_x = {},
             lualine_y = {},
             lualine_z = {},
@@ -71,6 +56,6 @@ return {
         opts.tabline = {}
         opts.winbar = {}
         opts.inactive_winbar = {}
-        opts.extensions = { "neo-tree", "lazy" }
+        opts.extensions = {}
     end,
 }
